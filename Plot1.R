0c69c5adff3024e280c53b91522b26a3acee2b8b
sinhaa8 <sinhaa@vmware.com>
2017-04-03 16:13:38 +0530

    Add files via upload

---
+++ Plot1.R
@@ -0,0 +1,12 @@
+getwd()
+setwd("C:/Ashish_Data/Work/Parag/coursera/Module4")
+dataFile <- "./exdata%2Fdata%2Fhousehold_power_consumption/household_power_consumption.txt"
+data <- read.table(dataFile, header=TRUE, sep=";", stringsAsFactors=FALSE, dec=".")
+subSetData <- data[data$Date %in% c("1/2/2007","2/2/2007") ,]
+
+#str(subSetData)
+globalActivePower <- as.numeric(subSetData$Global_active_power)
+png("plot1.png", width=480, height=480)
+hist(globalActivePower, col="red", main="Global Active Power", xlab="Global Active Power (kilowatts)")
+dev.off()
+dev.on()
